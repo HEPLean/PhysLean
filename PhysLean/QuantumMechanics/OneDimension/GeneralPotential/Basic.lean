4ca79edd533f5e7f41ef649c5787fb08ca0b5094
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-03-25 05:32:26 -0400

    refactor: Lint

--- PhysLean/QuantumMechanics/OneDimension/GeneralPotential/Basic.lean
+++ PhysLean/QuantumMechanics/OneDimension/GeneralPotential/Basic.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Ammar Husain. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Ammar Husain
 -/
-import PhysLean.Mathematics.SpecialFunctions.PhyscisistsHermite
+import PhysLean.Mathematics.SpecialFunctions.PhysHermite
 import PhysLean.QuantumMechanics.OneDimension.HilbertSpace.Basic
 import Mathlib.Algebra.Order.GroupWithZero.Unbundled
 import Mathlib.Analysis.Calculus.Deriv.Add
@@ -29,56 +29,62 @@ open MeasureTheory
 noncomputable def momentumOperator (ℏ : ℝ) (ψ : ℝ → ℂ) : ℝ → ℂ :=
   fun x => - Complex.I * ℏ * deriv ψ x
 
-lemma fun_add {α : Type*} (f g : α → ℂ):
+private lemma fun_add {α : Type*} (f g : α → ℂ):
   (fun x => f x) + (fun x => g x) = fun x => f x + g x := by
   rfl
 
-lemma fun_smul (a1: ℂ) (f : ℝ → ℂ): (a1 • fun x => f x) = (fun x => a1*(f x)) := by
+private lemma fun_smul (a1: ℂ) (f : ℝ → ℂ): (a1 • fun x => f x) = (fun x => a1*(f x)) := by
     rfl
 
-lemma momentum_linear (ℏ : ℝ) (a1 a2 : ℂ) (ψ1 ψ2 : ℝ → ℂ)
-  (hψ1_x: Differentiable ℝ ψ1) (hψ2_x: Differentiable ℝ ψ2) :
-  momentumOperator ℏ ((a1 • ψ1) + (a2 • ψ2)) = a1 • momentumOperator ℏ ψ1 + a2 • momentumOperator ℏ ψ2 := by
-    unfold momentumOperator
-    have h1: (a1 • fun x => -Complex.I * ↑ℏ * deriv ψ1 x) = (fun x => a1*(-Complex.I * ↑ℏ * deriv ψ1 x)) := by
-      rfl
-    have h2: (a2 • fun x => -Complex.I * ↑ℏ * deriv ψ2 x) = (fun x => a2*(-Complex.I * ↑ℏ * deriv ψ2 x)) := by
-      rfl
-    rw [h1,h2]
-    rw [fun_add ((fun x => a1 * (-Complex.I * ↑ℏ * deriv ψ1 x))) _]
-    ext x
-    have ha1ψ1_x: Differentiable ℝ (a1 •ψ1) := by
-      exact (hψ1_x).const_smul a1
-    have ha2ψ2_x: Differentiable ℝ (a2 •ψ2) := by
-      exact (hψ2_x).const_smul a2
-    have h: deriv ((a1 •ψ1) + (a2 •ψ2)) x = deriv (fun y => ((a1 •ψ1) y) + ((a2 •ψ2) y)) x := by
-      rfl
-    rw [h]
-    rw [deriv_add]
-    have ht1: deriv (a1 •ψ1) x = deriv (fun y => (a1 •ψ1 y)) x := by
-      rfl
-    have ht2: deriv (a2 •ψ2) x = deriv (fun y => (a2 •ψ2 y)) x := by
-      rfl
-    rw [ht1,ht2]
-    rw [deriv_const_smul, deriv_const_smul]
-    rw [mul_add]
-    simp only [mul_comm, mul_assoc]
-    rw [← mul_assoc,← mul_assoc]
-    rw [← mul_assoc a1 _ _]
-    rw [← mul_assoc a2 _ _]
-    rw [mul_assoc]
-    rw [mul_assoc]
+lemma momentumOperator_linear (ℏ : ℝ) (a1 a2 : ℂ) (ψ1 ψ2 : ℝ → ℂ)
+    (hψ1_x: Differentiable ℝ ψ1) (hψ2_x: Differentiable ℝ ψ2) :
+    momentumOperator ℏ ((a1 • ψ1) + (a2 • ψ2)) =
+    a1 • momentumOperator ℏ ψ1 + a2 • momentumOperator ℏ ψ2 := by
+  unfold momentumOperator
+  have h1: (a1 • fun x => -Complex.I * ↑ℏ * deriv ψ1 x) =
+      (fun x => a1*(-Complex.I * ↑ℏ * deriv ψ1 x)) := by
     rfl
-    exact hψ2_x x
-    exact hψ1_x x
-    exact (hψ1_x x).const_smul a1
-    exact (hψ2_x x).const_smul a2
+  have h2: (a2 • fun x => -Complex.I * ↑ℏ * deriv ψ2 x) =
+      (fun x => a2*(-Complex.I * ↑ℏ * deriv ψ2 x)) := by
+    rfl
+  rw [h1,h2]
+  rw [fun_add ((fun x => a1 * (-Complex.I * ↑ℏ * deriv ψ1 x))) _]
+  ext x
+  have ha1ψ1_x: Differentiable ℝ (a1 •ψ1) := by
+    exact (hψ1_x).const_smul a1
+  have ha2ψ2_x: Differentiable ℝ (a2 •ψ2) := by
+    exact (hψ2_x).const_smul a2
+  have h: deriv ((a1 •ψ1) + (a2 •ψ2)) x = deriv (fun y => ((a1 •ψ1) y) + ((a2 •ψ2) y)) x := by
+    rfl
+  rw [h]
+  rw [deriv_add]
+  have ht1: deriv (a1 •ψ1) x = deriv (fun y => (a1 •ψ1 y)) x := by
+    rfl
+  have ht2: deriv (a2 •ψ2) x = deriv (fun y => (a2 •ψ2 y)) x := by
+    rfl
+  rw [ht1,ht2]
+  rw [deriv_const_smul, deriv_const_smul]
+  rw [mul_add]
+  simp only [mul_comm, mul_assoc]
+  rw [← mul_assoc,← mul_assoc]
+  rw [← mul_assoc a1 _ _]
+  rw [← mul_assoc a2 _ _]
+  rw [mul_assoc]
+  rw [mul_assoc]
+  rfl
+  exact hψ2_x x
+  exact hψ1_x x
+  exact (hψ1_x x).const_smul a1
+  exact (hψ2_x x).const_smul a2
 
-lemma momentum2_linear (ℏ : ℝ) (a1 a2 : ℂ) (ψ1 ψ2 : ℝ → ℂ)
-  (hψ1_x: Differentiable ℝ ψ1) (hψ2_x: Differentiable ℝ ψ2)
-  (hψ1_xx: Differentiable ℝ (momentumOperator ℏ ψ1)) (hψ2_xx: Differentiable ℝ (momentumOperator ℏ ψ2)):
-  momentumOperator ℏ (momentumOperator ℏ ((a1 • ψ1) + (a2 • ψ2))) = a1 • (momentumOperator ℏ (momentumOperator ℏ ψ1)) + a2 • (momentumOperator ℏ (momentumOperator ℏ ψ2)) := by
-  rw [momentum_linear, momentum_linear]
+lemma momentumOperator_sq_linear (ℏ : ℝ) (a1 a2 : ℂ) (ψ1 ψ2 : ℝ → ℂ)
+    (hψ1_x: Differentiable ℝ ψ1) (hψ2_x: Differentiable ℝ ψ2)
+    (hψ1_xx: Differentiable ℝ (momentumOperator ℏ ψ1))
+    (hψ2_xx: Differentiable ℝ (momentumOperator ℏ ψ2)):
+    momentumOperator ℏ (momentumOperator ℏ ((a1 • ψ1) + (a2 • ψ2))) =
+    a1 • (momentumOperator ℏ (momentumOperator ℏ ψ1)) +
+    a2 • (momentumOperator ℏ (momentumOperator ℏ ψ2)) := by
+  rw [momentumOperator_linear, momentumOperator_linear]
   exact hψ1_xx
   exact hψ2_xx
   exact hψ1_x
@@ -91,12 +97,13 @@ noncomputable def positionOperator (ψ : ℝ → ℂ) : ℝ → ℂ :=
 
 /-- The potential operator is defined as the map from `ℝ → ℂ` to `ℝ → ℂ` taking
   `ψ` to `V(x) ψ`. -/
-noncomputable def V_operator (V : ℝ → ℝ) (ψ : ℝ → ℂ) : ℝ → ℂ :=
+noncomputable def potentialOperator (V : ℝ → ℝ) (ψ : ℝ → ℂ) : ℝ → ℂ :=
   fun x => V x * ψ x
 
-lemma V_linear (V: ℝ → ℝ) (a1 a2 : ℂ) (ψ1 ψ2 : ℝ → ℂ):
-  V_operator V ((a1 • ψ1) + (a2 • ψ2)) = a1 • V_operator V ψ1 + a2 • V_operator V ψ2 := by
-  unfold V_operator
+lemma potentialOperator_linear (V: ℝ → ℝ) (a1 a2 : ℂ) (ψ1 ψ2 : ℝ → ℂ):
+    potentialOperator V ((a1 • ψ1) + (a2 • ψ2)) =
+    a1 • potentialOperator V ψ1 + a2 • potentialOperator V ψ2 := by
+  unfold potentialOperator
   have h1: (a1 • fun x => V x * ψ1 x) = (fun x => a1*(V x * ψ1 x)) := by
     rfl
   have h2: (a2 • fun x => V x * ψ2 x) = (fun x => a2*(V x * ψ2 x)) := by
@@ -131,8 +138,8 @@ namespace GeneralPotential
 
 variable (Q : GeneralPotential)
 
-/-- For a 1D quantum mechanical system in potential `V`, the Schrodinger Operator corresponding to it
-  is defined as the function from `ℝ → ℂ` to `ℝ → ℂ` taking
+/-- For a 1D quantum mechanical system in potential `V`, the Schrodinger Operator corresponding
+  to it is defined as the function from `ℝ → ℂ` to `ℝ → ℂ` taking
 
   `ψ ↦ - ℏ^2 / (2 * m) * ψ'' + V(x) * ψ`. -/
 noncomputable def schrodingerOperator (ψ : ℝ → ℂ) : ℝ → ℂ :=
@@ -142,15 +149,20 @@ lemma eval_add (f g : ℝ → ℂ):
   (f + g) x = f x + g x := by
   rfl
 
-lemma schrodinger_linear (a1 a2 : ℂ) (ψ1 ψ2 : ℝ → ℂ)
-  (hψ1_x: Differentiable ℝ ψ1) (hψ2_x: Differentiable ℝ ψ2)
-  (hψ1_xx: Differentiable ℝ (momentumOperator Q.ℏ ψ1)) (hψ2_xx: Differentiable ℝ (momentumOperator Q.ℏ ψ2)):
-  schrodingerOperator Q ((a1 • ψ1) + (a2 • ψ2)) = a1 • schrodingerOperator Q ψ1 + a2 • schrodingerOperator Q ψ2 := by
+lemma schrodingerOperator_linear (a1 a2 : ℂ) (ψ1 ψ2 : ℝ → ℂ)
+    (hψ1_x: Differentiable ℝ ψ1) (hψ2_x: Differentiable ℝ ψ2)
+    (hψ1_xx: Differentiable ℝ (momentumOperator Q.ℏ ψ1))
+    (hψ2_xx: Differentiable ℝ (momentumOperator Q.ℏ ψ2)):
+    schrodingerOperator Q ((a1 • ψ1) + (a2 • ψ2)) =
+    a1 • schrodingerOperator Q ψ1 + a2 • schrodingerOperator Q ψ2 := by
   unfold schrodingerOperator
-  rw [momentum2_linear]
-  rw [fun_smul a1 (fun x => 1 / (2 * Q.m) * (momentumOperator Q.ℏ (momentumOperator Q.ℏ ψ1) x) + (Q.V x) *  ψ1 x)]
-  rw [fun_smul a2 (fun x => 1 / (2 * Q.m) * (momentumOperator Q.ℏ (momentumOperator Q.ℏ ψ2) x) + (Q.V x) *  ψ2 x)]
-  rw [fun_add (fun x => a1*(1 / (2 * Q.m) * (momentumOperator Q.ℏ (momentumOperator Q.ℏ ψ1) x) + (Q.V x) *  ψ1 x)) _]
+  rw [momentumOperator_sq_linear]
+  rw [fun_smul a1 (fun x => 1 / (2 * Q.m) *
+    (momentumOperator Q.ℏ (momentumOperator Q.ℏ ψ1) x) + (Q.V x) *  ψ1 x)]
+  rw [fun_smul a2 (fun x => 1 / (2 * Q.m) *
+    (momentumOperator Q.ℏ (momentumOperator Q.ℏ ψ2) x) + (Q.V x) *  ψ2 x)]
+  rw [fun_add (fun x => a1*(1 / (2 * Q.m) *
+    (momentumOperator Q.ℏ (momentumOperator Q.ℏ ψ1) x) + (Q.V x) *  ψ1 x)) _]
   ext x
   rw [eval_add, mul_add]
   rw [eval_add, mul_add]
@@ -186,7 +198,7 @@ lemma schrodinger_linear (a1 a2 : ℂ) (ψ1 ψ2 : ℝ → ℂ)
 
 structure BoundedPotential where
   boundless: GeneralPotential
-  hbound_left: ∀E,∃ R,∀z < -R, boundless.V z > E
-  hbound_right: ∀E,∃ R,∀z > R, boundless.V z > E
+  hbound_left: ∀ E,∃ R,∀z < -R, boundless.V z > E
+  hbound_right: ∀ E,∃ R,∀z > R, boundless.V z > E
 
 namespace BoundedPotential
