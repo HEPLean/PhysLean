1387279f14961ce1c6501847bf91abe9ed0bc3c2
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-04-14 11:32:48 +0000

    feat: Correct copyright

--- PhysLean/Relativity/SpaceTime/TimeSlice.lean
+++ PhysLean/Relativity/SpaceTime/TimeSlice.lean
@@ -1,13 +1,9 @@
 /-
-Copyright (c) 2024 Joseph Tooby-Smith. All rights reserved.
+Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Tooby-Smith
 -/
-import PhysLean.Relativity.Tensors.RealTensor.Vector.Basic
-import PhysLean.ClassicalMechanics.Space.Basic
-import PhysLean.ClassicalMechanics.Time.Basic
 import PhysLean.Relativity.SpaceTime.Basic
-import PhysLean.Meta.Informal.SemiFormal
 /-!
 # Time slice
 
