eb8c5e1551bb0fd52a3dc80039eea5d70b9c79f8
Jeremy Lindsay <jeremy@jeremylindsay.com>
2025-04-23 00:08:15 +1000

    added definition of restricted Lorentz group as a set

--- PhysLean/Relativity/Lorentz/Group/Restricted.lean
+++ PhysLean/Relativity/Lorentz/Group/Restricted.lean
@@ -4,6 +4,8 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Tooby-Smith
 -/
 import PhysLean.Relativity.Lorentz.Group.Orthochronous
+import PhysLean.Relativity.Lorentz.Group.Proper
+import PhysLean.Relativity.Lorentz.Group.Basic
 import PhysLean.Meta.Informal.Basic
 /-!
 # The Restricted Lorentz Group
@@ -20,8 +22,13 @@ TODO "6VZNU" "Prove restricted Lorentz group equivalent to connected component o
 namespace LorentzGroup
 
 /-- The subgroup of the Lorentz group consisting of elements which are proper and orthochronous. -/
-informal_definition Restricted where
-  deps := [``LorentzGroup, ``IsProper, ``IsOrthochronous]
-  tag := "6VZN7"
+-- informal_definition Restricted where
+--   deps := [``LorentzGroup, ``IsProper, ``IsOrthochronous]
+--   tag := "6VZN7"
+
+/- The restricted Lorentz group is the subset of the Lorentz group which is proper
+and orthochronous. -/
+def Restricted (d : ℕ) : Set (LorentzGroup d) :=
+  { Λ : LorentzGroup d | IsProper Λ ∧ IsOrthochronous Λ }
 
 end LorentzGroup
