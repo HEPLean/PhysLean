952a5748ea67475a7cf25310cbc0551d61435937
Jeremy Lindsay <jeremy@jeremylindsay.com>
2025-04-23 14:05:32 +1000

    simplified `mul_mem'`; added `mul_proper_of_proper_proper` to Lorentz/Group/Proper.lean

--- PhysLean/Relativity/Lorentz/Group/Restricted.lean
+++ PhysLean/Relativity/Lorentz/Group/Restricted.lean
@@ -42,11 +42,8 @@ instance restrictedLorentzGroupIsSubgroup {d : ℕ} : Subgroup (LorentzGroup d) 
   ⟩
   mul_mem' := by
     rintro Λ₁ Λ₂ ⟨Λ₁_proper, Λ₁_ortho⟩ ⟨Λ₂_proper, Λ₂_ortho⟩
-    have h_det : det (Λ₁.1 * Λ₂.1) = 1 := by
-      rw [IsProper] at Λ₁_proper Λ₂_proper
-      rw [det_mul, Λ₁_proper, Λ₂_proper, mul_one]
     exact ⟨
-      by rw [IsProper]; exact h_det,
+      by exact mul_proper_of_proper_proper Λ₁_proper Λ₂_proper,
       by exact mul_othchron_of_othchron_othchron Λ₁_ortho Λ₂_ortho
     ⟩
   inv_mem' := sorry
