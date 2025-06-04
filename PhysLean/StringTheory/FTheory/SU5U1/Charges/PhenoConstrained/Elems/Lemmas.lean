1cb5be24ae10d74aadf30c8c2f10c541571bebdd
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-06-04 11:43:18 +0000

    refactor: Update Lemmas with lower recursion

--- PhysLean/StringTheory/FTheory/SU5U1/Charges/PhenoConstrained/Elems/Lemmas.lean
+++ PhysLean/StringTheory/FTheory/SU5U1/Charges/PhenoConstrained/Elems/Lemmas.lean
@@ -20,17 +20,25 @@ open ChargeProfile
 open CodimensionOneConfig
 open Tree Leaf Twig Branch Trunk
 
-set_option maxRecDepth 5000 in
+
+set_option maxRecDepth 2400 in
 lemma card_Q5_le_three_of_nonPhenoConstrainedCharges_of_same :
     ∀ x ∈ (nonPhenoConstrainedCharges same).toMultiset, x.2.2.1.card ≤ 4 := by
-  decide
+  intro x hx
+  by_contra hnot
+  have hx : x ∈ (nonPhenoConstrainedCharges same).toMultiset.filter fun x => 4 < x.2.2.1.card := by
+    simp_all
+  have hl : Multiset.filter (fun x => 4 < x.2.2.1.card)
+    (nonPhenoConstrainedCharges same).toMultiset  = ∅ := by rfl
+  rw [hl] at hx
+  simp at hx
 
-set_option maxRecDepth 2000 in
+set_option maxRecDepth 1700 in
 lemma card_Q5_le_three_of_nonPhenoConstrainedCharges_of_nearestNeighbor :
     ∀ x ∈ (nonPhenoConstrainedCharges nearestNeighbor).toMultiset, x.2.2.1.card ≤ 4 := by
   decide
 
-set_option maxRecDepth 2000 in
+set_option maxRecDepth 1600 in
 lemma card_Q5_le_three_of_nonPhenoConstrainedCharges_of_nextToNearestNeighbor :
     ∀ x ∈ (nonPhenoConstrainedCharges nextToNearestNeighbor).toMultiset, x.2.2.1.card ≤ 4 := by
   decide
@@ -44,10 +52,17 @@ lemma card_Q5_le_three_of_nonPhenoConstrainedCharges (I : CodimensionOneConfig) 
     card_Q5_le_three_of_nonPhenoConstrainedCharges_of_nextToNearestNeighbor
 
 
-set_option maxRecDepth 5000 in
+set_option maxRecDepth 2400 in
 lemma card_Q10_le_three_of_nonPhenoConstrainedCharges_of_same :
     ∀ x ∈ (nonPhenoConstrainedCharges same).toMultiset, x.2.2.2.card ≤ 3 := by
-  decide
+  intro x hx
+  by_contra hnot
+  have hx : x ∈ (nonPhenoConstrainedCharges same).toMultiset.filter fun x => 3 < x.2.2.2.card := by
+    simp_all
+  have hl : Multiset.filter (fun x => 3 < x.2.2.2.card)
+    (nonPhenoConstrainedCharges same).toMultiset  = ∅ := by rfl
+  rw [hl] at hx
+  simp at hx
 
 set_option maxRecDepth 2000 in
 lemma card_Q10_le_three_of_nonPhenoConstrainedCharges_of_nearestNeighbor :
