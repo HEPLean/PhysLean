902316c14ed15c7c16dbde5befc62aea35577426
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-06-04 06:47:49 +0000

    feat: Quanta in SU(5)+U(1) F-theory.

---
+++ PhysLean/StringTheory/FTheory/SU5U1/Charges/PhenoConstrained/Elems/Lemmas.lean
@@ -0,0 +1,37 @@
+/-
+Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Joseph Tooby-Smith
+-/
+import PhysLean.StringTheory.FTheory.SU5U1.Charges.PhenoConstrained.Elems.Basic
+/-!
+
+# Other lemmas related to nonPhenoConstrainedCharges
+
+-/
+namespace FTheory
+
+namespace SU5U1
+
+variable {I : CodimensionOneConfig}
+namespace Charges
+open PotentialTerm
+open ChargeProfile
+open CodimensionOneConfig
+open Tree Leaf Twig Branch Trunk
+
+set_option maxRecDepth 2000 in
+lemma card_Q5_le_three_of_nonPhenoConstrainedCharges :
+    ∀ I, ∀ x ∈ (nonPhenoConstrainedCharges I).toMultiset, x.2.2.1.card ≤ 3 := by
+  decide
+
+set_option maxRecDepth 2000 in
+lemma card_Q10_le_three_of_nonPhenoConstrainedCharges :
+    ∀ I, ∀ x ∈ (nonPhenoConstrainedCharges I).toMultiset, x.2.2.2.card ≤ 3 := by
+  decide
+
+end Charges
+
+end SU5U1
+
+end FTheory
