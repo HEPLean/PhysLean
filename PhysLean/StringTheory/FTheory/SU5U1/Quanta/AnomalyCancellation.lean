b2b636d1e342ea8820d38d90b90baa48f90e4564
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-06-06 07:24:52 +0000

    refactor: Lint

--- PhysLean/StringTheory/FTheory/SU5U1/Quanta/AnomalyCancellation.lean
+++ PhysLean/StringTheory/FTheory/SU5U1/Quanta/AnomalyCancellation.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Tooby-Smith
 -/
 import PhysLean.StringTheory.FTheory.SU5U1.Quanta.FromParts
-import PhysLean.StringTheory.FTheory.SU5U1.Charges.PhenoConstrained.Elems.Basic
 /-!
 
 # Anomaly cancellation
