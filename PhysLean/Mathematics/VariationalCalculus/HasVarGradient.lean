e4e5dbc2a1cbad9f11cee0d3499f9c8a2049d7d0
lecopivo <skrivantomas@seznam.cz>
2025-06-20 16:44:51 +0200

    fix file renaming

--- PhysLean/Mathematics/VariationalCalculus/HasVarGradient.lean
+++ PhysLean/Mathematics/VariationalCalculus/HasVarGradient.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Tomas Skrivan. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Tomas Skrivan, Joseph Tooby-Smith
 -/
-import PhysLean.Mathematics.VariationalCalculus.HasVarRevDeriv
+import PhysLean.Mathematics.VariationalCalculus.HasVarAdjDeriv
 /-!
 
 # Variational gradient
