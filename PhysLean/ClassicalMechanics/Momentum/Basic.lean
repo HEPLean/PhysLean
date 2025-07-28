552870470f5174de8487e8eff2431289dac2ccf0
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-07-28 15:20:05 +0000

    refactor: Lint

--- PhysLean/ClassicalMechanics/Momentum/Basic.lean
+++ PhysLean/ClassicalMechanics/Momentum/Basic.lean
@@ -16,6 +16,7 @@ in `d`-dimensional space, in an arbitrary (but given) set of units.
 /-- Momentum in `d`-dimensional space in an arbitary, but given, set of units.
   In `(3+1)d` space time this corresponds to `3`-momentum not `4`-momentum. -/
 structure Momentum (d : ℕ := 3) where
+  /-- The underlying vector of a momentum. -/
   val : Fin d → ℝ
 
 TODO "IO7OT" "On the type `Momentum` define the instance of an inner product space."
