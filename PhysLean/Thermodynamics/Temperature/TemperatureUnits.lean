0faacf28c8efaf98345e3071f90ea55f3a5b7fc8
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-07-30 12:57:26 +0000

    feat: Modify unit ratios to NNReal

--- PhysLean/Thermodynamics/Temperature/TemperatureUnits.lean
+++ PhysLean/Thermodynamics/Temperature/TemperatureUnits.lean
@@ -26,6 +26,8 @@ existence of the temperature unit of kelvin, and construct all other temperature
 
 -/
 
+open NNReal
+
 /-- The choices of translationally-invariant metrics on the temperature-manifold.
   Such a choice corresponds to a choice of units for temperature. -/
 structure TemperatureUnit where
@@ -50,11 +52,11 @@ instance : Inhabited TemperatureUnit where
 
 -/
 
-noncomputable instance : HDiv TemperatureUnit TemperatureUnit ℝ where
-  hDiv x t := x.val / t.val
+noncomputable instance : HDiv TemperatureUnit TemperatureUnit ℝ≥0 where
+  hDiv x t := ⟨x.val / t.val, div_nonneg (le_of_lt x.val_pos) (le_of_lt t.val_pos)⟩
 
 lemma div_eq_val (x y : TemperatureUnit) :
-    x / y = x.val / y.val := rfl
+    x / y = (⟨x.val / y.val, div_nonneg (le_of_lt x.val_pos) (le_of_lt y.val_pos)⟩ : ℝ≥0) := rfl
 
 @[simp]
 lemma div_pos (x y : TemperatureUnit) :
@@ -63,11 +65,11 @@ lemma div_pos (x y : TemperatureUnit) :
 
 @[simp]
 lemma div_self (x : TemperatureUnit) :
-    x / x = (1 : ℝ) := by
+    x / x = (1 : ℝ≥0) := by
   simp [div_eq_val, x.val_neq_zero]
 
 lemma div_symm (x y : TemperatureUnit) :
-    x / y = (y / x)⁻¹ := by
+    x / y = (y / x)⁻¹ := NNReal.eq <| by
   rw [div_eq_val, inv_eq_one_div, div_eq_val]
   simp
 
@@ -83,7 +85,7 @@ def scale (r : ℝ) (x : TemperatureUnit) (hr : 0 < r := by norm_num) : Temperat
 
 @[simp]
 lemma scale_div_self (x : TemperatureUnit) (r : ℝ) (hr : 0 < r) :
-    scale r x hr / x = r := by
+    scale r x hr / x = (⟨r, le_of_lt hr⟩ : ℝ≥0) := by
   simp [scale, div_eq_val]
 
 @[simp]
@@ -92,7 +94,8 @@ lemma scale_one (x : TemperatureUnit) : scale 1 x = x := by
 
 @[simp]
 lemma scale_div_scale (x1 x2 : TemperatureUnit) {r1 r2 : ℝ} (hr1 : 0 < r1) (hr2 : 0 < r2) :
-    scale r1 x1 hr1 / scale r2 x2 hr2 = r1 / r2 * (x1 / x2) := by
+    scale r1 x1 hr1 / scale r2 x2 hr2 = (⟨r1, le_of_lt hr1⟩ / ⟨r2, le_of_lt hr2⟩) * (x1 / x2) := by
+  refine NNReal.eq ?_
   simp [scale, div_eq_val]
   field_simp
 
