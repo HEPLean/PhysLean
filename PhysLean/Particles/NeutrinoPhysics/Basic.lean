ac1e9addbd3f735d5f48ab5f074aacfe96daa346
PrabhodaCS <prabhodacs@gmail.com>
2025-10-16 00:23:08 +0530

    Fixed linting errors

--- PhysLean/Particles/NeutrinoPhysics/Basic.lean
+++ PhysLean/Particles/NeutrinoPhysics/Basic.lean
@@ -18,11 +18,9 @@ open Matrix Complex
 noncomputable section
 
 /-- diagonal phase matrix from a real-valued function on indices -/
-
 @[simp]
 def diagPhase (θ : Fin 3 → ℝ) : Matrix (Fin 3) (Fin 3) ℂ :=
   λ i j => if i = j then cexp (I * θ i) else 0
-
 
 /-- lemma stating that the diagonal phase matrix with all zeros is the identity matrix -/
 lemma diagPhase_zero:
@@ -30,7 +28,8 @@ lemma diagPhase_zero:
   ext i j
   fin_cases i <;> fin_cases j <;> simp []
 
-/-- lemma stating that the Hermitian conjugate of diagPhase diag(+iθ_i) is just diagPhase with entries diag(-iθ_i)-/
+/-- lemma stating that the Hermitian conjugate of diagPhase diag(+iθ_i)
+is just diagPhase with entries diag(-iθ_i)-/
 lemma diagPhase_star (θ : Fin 3 → ℝ) :
   (diagPhase θ )ᴴ = diagPhase (- θ) := by
   ext i j
@@ -50,20 +49,30 @@ def diagPhase_unitary (θ : Fin 3 → ℝ) : unitaryGroup (Fin 3) ℂ :=
   rw[Matrix.mem_unitaryGroup_iff]
   change _ * (diagPhase θ)ᴴ = 1
   ext i j
-  fin_cases i <;> fin_cases j <;> simp [diagPhase, Matrix.mul_apply, Matrix.conjTranspose_apply, ← exp_conj, ← exp_add]
+  fin_cases i <;> fin_cases j <;> simp [diagPhase,
+  Matrix.mul_apply,
+  Matrix.conjTranspose_apply,
+  ← exp_conj,
+  ← exp_add]
   ⟩
 
-/-- Given three reals `a b c` the Lepton phase shift matrix as a `3×3` complex matrix, which dictate the phase shift freedom of the charged lepton sector.
+/-- The Lepton phase shift matrix as a `3×3` complex matrix, given three reals `a b c`.
+This dictates the phase shift freedom of the charged lepton sector.
+-/
 
-Given three reals `d e f` the neutrino phase shift matrix as a `3×3` complex matrix, which dictate the phase shift freedom of the neutrino sector (If neutrinos are Dirac particles).
-
-If neutrinos are Majorana particles, then the neutrino phase shift matrix is physical, and cannot be absorbed into the definition of the neutrino fields.
- -/
 def LeptonPhaseShift (a b c : ℝ) : Matrix (Fin 3) (Fin 3) ℂ :=
   diagPhase (fun i => if i = 0 then a else if i = 1 then b else c)
 
+/-- The neutrino phase shift matrix as a `3×3` complex matrix, given three reals `d e f`.
+This dictates the phase shift freedom of the neutrino sector (If neutrinos are Dirac).
+-/
+
 def NeutrinoPhaseShift (d e f : ℝ) : Matrix (Fin 3) (Fin 3) ℂ :=
   diagPhase (fun i => if i = 0 then d else if i = 1 then e else f)
+
+/-- If neutrinos are Majorana particles, then the neutrino phase shift matrix is physical,
+and cannot be absorbed into the definition of the neutrino fields.
+-/
 
 def MajoranaPhaseMatrix (α1 α2 : ℝ) : Matrix (Fin 3) (Fin 3) ℂ :=
   diagPhase (fun i => if i = 0 then 0 else if i = 1 then α1/2 else α2/2)
