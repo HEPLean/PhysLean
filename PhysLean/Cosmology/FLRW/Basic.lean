17ce16ea16c325622bce4f37f72f22e24a487ca8
Luis Gabriel Bariuan <luisgbariuan@gmail.com>
2025-10-21 16:09:40 -0700

    Formalized basic definitions in FLRW. Introduced intermediatte lemmata

--- PhysLean/Cosmology/FLRW/Basic.lean
+++ PhysLean/Cosmology/FLRW/Basic.lean
@@ -6,8 +6,6 @@ Authors: Joseph Tooby-Smith, Luis Gabriel C. Bariuan
 import Mathlib
 import PhysLean.Meta.Informal.SemiFormal
 import PhysLean.SpaceAndTime.Space.Basic
-
-
 open Filter
 open scoped Topology
 /-!
@@ -32,10 +30,7 @@ inductive SpatialGeometry : Type where
   | Flat (k : ℝ) (h: k = 0)
   | Saddle (k : ℝ) (h : k > 0)
 
-
-
 namespace SpatialGeometry
-
 /-- For `s` corresponding to
 - `Spherical k`, `S s r = k * sin (r / k)`
 - `Flat`, `S s r = r`,
@@ -46,24 +41,20 @@ noncomputable def S (s : SpatialGeometry) : ℝ → ℝ :=
   fun r =>
     match s with
     | SpatialGeometry.Spherical k _ => k * Real.sin (r / k)
-    | SpatialGeometry.Flat _ _      => r
-    | SpatialGeometry.Saddle k _    => k * Real.sinh (r / k)
-
+    | SpatialGeometry.Flat _ _ => r
+    | SpatialGeometry.Saddle k _ => k * Real.sinh (r / k)
 /-- The limit of `S (Saddle k) r` as `k → ∞` is equal to `S (Flat) r`. -/
-
---/ First show that k*sinh(r/k) = sinh(r/k)/(1/k) pointwise.
+/-- First show that `k * sinh(r / k) = sinh(r / k) / (1 / k)` pointwise. -/
 lemma mul_sinh_as_div (r k : ℝ) :
   k * Real.sinh (r / k) = Real.sinh (r / k) / (1 / k) := by field_simp
-
 @[sorryful]
---/ First, show that limit of sinh(r * x) / x is r at the limit x goes to zero. Then the next theorem will address the rewrite using Filter.Tendsto.comp
-lemma tendsto_sinh_rx_over_x (r : ℝ) : Tendsto (fun x : ℝ => Real.sinh (r * x) / x) (𝓝[≠] 0) (𝓝 r) := by
-  sorry
+/-- First, show that limit of `sinh(r * x) / x` is r at the limit x goes to zero.
+Then the next theorem will address the rewrite using Filter.Tendsto.comp -/
+lemma tendsto_sinh_rx_over_x (r : ℝ) :
+  Tendsto (fun x : ℝ => Real.sinh (r * x) / x) (𝓝[≠] 0) (𝓝 r) := by sorry
 @[sorryful]
-lemma limit_S_saddle(r : ℝ) : Tendsto (fun k : ℝ => k * Real.sinh (r / k)) atTop (𝓝 r) := by
-  sorry
-
-
+lemma limit_S_saddle(r : ℝ) :
+  Tendsto (fun k : ℝ => k * Real.sinh (r / k)) atTop (𝓝 r) := by sorry
 /-- The limit of `S (Sphere k) r` as `k → ∞` is equal to `S (Flat) r`. -/
 informal_lemma limit_S_sphere where
   deps := []
