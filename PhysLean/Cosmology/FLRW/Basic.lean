02a7c00d023cd8eaf8abcf5be1dc410d1fbb34e9
Luis Gabriel Bariuan <luisgbariuan@gmail.com>
2025-10-25 23:00:49 -0700

    Added corresponding lemmas to spherical geometry case. Defined first order Friedmann equation, hubble constant, and the deceleration parameter.

--- PhysLean/Cosmology/FLRW/Basic.lean
+++ PhysLean/Cosmology/FLRW/Basic.lean
@@ -58,10 +58,20 @@ lemma tendsto_sinh_rx_over_x (r : ℝ) :
 lemma limit_S_saddle(r : ℝ) :
     Tendsto (fun k : ℝ => k * Real.sinh (r / k)) atTop (𝓝 r) := by sorry
 
-/-- The limit of `S (Sphere k) r` as `k → ∞` is equal to `S (Flat) r`. -/
-informal_lemma limit_S_sphere where
-  deps := []
-  tag := "62A4R"
+/-- The limit of `S (Sphere k) r` as `k → ∞` is equal to `S (Flat) r`.
+First show that `k * sinh(r / k) = sin(r / k) / (1 / k)` pointwise. -/
+lemma mul_sin_as_div (r k : ℝ) :
+    k * Real.sin (r / k) = Real.sin (r / k) / (1 / k) := by field_simp
+
+/-- First, show that limit of `sin(r * x) / x` is r at the limit x goes to zero.
+Then the next theorem will address the rewrite using Filter.Tendsto.comp -/
+@[sorryful]
+lemma tendsto_sin_rx_over_x (r : ℝ) :
+    Tendsto (fun x : ℝ => Real.sin (r * x) / x) (𝓝[≠] 0) (𝓝 r) := by sorry
+
+@[sorryful]
+lemma limit_S_sphere(r : ℝ) :
+    Tendsto (fun k : ℝ => k * Real.sin (r / k)) atTop (𝓝 r) := by sorry
 
 end SpatialGeometry
 
@@ -78,15 +88,38 @@ def FLRW : Type := sorry
 
 namespace FLRW
 
+/-!
+Formalization sketch for the first-order Friedmann equation,
+using units `c = 1`.
+
+`a : ℝ → ℝ` is the FLRW scale factor as a function of cosmic time `t`.
+`ρ : ℝ → ℝ` is the total energy density.
+`k : ℝ` is the spatial curvature parameter.
+`Λ : ℝ` is the cosmological constant.
+`G : ℝ` is Newton's constant.
+`c : ℝ` is the speed of light. It may be set to 1 for convenience.
+We will leave it here for generality and accounting purposes.
+
+At time `t` the equation reads
+
+`(a' / a)^2 = (8πG/3) ρ − k c^2 / a^2 + Λ c^2 / 3`.
+-/
+
+namespace FriedmannEquation
+
+def FirstOrderFriedmann (a: ℝ → ℝ) (ρ k Λ G c: ℝ) (t : ℝ) : Prop :=
+    ((deriv a t / a t)^2
+      = ((8 * Real.pi * G) / 3) * ρ - k * c^2 / (a t)^2 + Λ * c ^2/ 3)
+
 /-- The hubble constant defined in terms of the scale factor
   as `(dₜ a) / a`.
 
   The notation `H` is used for the `hubbleConstant`.
 
   Semiformal implementation note: Implement also scoped notation. -/
-informal_definition hubbleConstant where
-  deps := []
-  tag := "6Z2NB"
+
+noncomputable def hubbleConstant (a : ℝ → ℝ) (t : ℝ) : ℝ :=
+    deriv a t / a t
 
 /-- The deceleration parameter defined in terms of the scale factor
   as `- (dₜdₜ a) a / (dₜ a)^2`.
@@ -94,9 +127,9 @@ informal_definition hubbleConstant where
   The notation `q` is used for the `decelerationParameter`.
 
   Semiformal implementation note: Implement also scoped notation. -/
-informal_definition decelerationParameter where
-  deps := []
-  tag := "6Z2UE"
+
+noncomputable def decelerationParameter (a : ℝ → ℝ) (t : ℝ) : ℝ :=
+    - (deriv (deriv a) t * a t) / (deriv a t)^2
 
 /-- The deceleration parameter is equal to `- (1 + (dₜ H)/H^2)`. -/
 informal_lemma decelerationParameter_eq_one_plus_hubbleConstant where
@@ -113,7 +146,7 @@ informal_lemma time_evolution_hubbleConstant where
 informal_lemma hubbleConstant_decrease_iff where
   deps := []
   tag := "6Z3FS"
-
+end FriedmannEquation
 end FLRW
 
 end Cosmology
