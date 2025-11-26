a9fbb70aad9c1a063ab4b514b6e92a23bc49ceca
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-11-26 06:12:41 +0000

    refactor: Remove transitive import

--- PhysLean/ClassicalMechanics/RigidBody/SolidSphere.lean
+++ PhysLean/ClassicalMechanics/RigidBody/SolidSphere.lean
@@ -4,7 +4,6 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Joseph Tooby-Smith
 -/
 import PhysLean.ClassicalMechanics.RigidBody.Basic
-import Mathlib.MeasureTheory.Measure.Lebesgue.VolumeOfBalls
 /-!
 
 # The solid sphere as a rigid body
