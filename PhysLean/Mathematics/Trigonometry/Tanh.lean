e6b5f02661aa2ba4e9b210a52e209ac79606419e
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-12-17 06:13:22 +0000

    refactor: Lint

--- PhysLean/Mathematics/Trigonometry/Tanh.lean
+++ PhysLean/Mathematics/Trigonometry/Tanh.lean
@@ -3,7 +3,6 @@ Copyright (c) 2025 Afiq Hatta. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Afiq Hatta
 -/
-import Mathlib.Analysis.Calculus.Deriv.Polynomial
 import Mathlib.Analysis.Distribution.SchwartzSpace
 import Mathlib.Topology.Algebra.Polynomial
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.DerivHyp
