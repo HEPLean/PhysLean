0427b710d529ed4facee78d767fcc9de5e128687
Nicola Bernini <nicola.bernini@gmail.com>
2026-01-04 11:45:20 +0100

    fix: Fixing Copyright

--- PhysLean/ClassicalMechanics/DampedHarmonicOscillator/Basic.lean
+++ PhysLean/ClassicalMechanics/DampedHarmonicOscillator/Basic.lean
@@ -1,7 +1,7 @@
 /-
-Copyright (c) 2025 Joseph Tooby-Smith. All rights reserved.
+Copyright (c) 2025 Nicola Bernini. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Joseph Tooby-Smith
+Authors: Nicola Bernini
 -/
 import PhysLean.Meta.Informal.SemiFormal
 import PhysLean.ClassicalMechanics.EulerLagrange
