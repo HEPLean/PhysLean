cd565c10e8ac7856617f13c75f0b16cd0e5975c9
ZhiKai Pong <zhikaip@gmail.com>
2026-02-09 20:24:46 +0000

    remove sorryful attribute

--- PhysLean/QFT/PerturbationTheory/WickContraction/ExtractEquiv.lean
+++ PhysLean/QFT/PerturbationTheory/WickContraction/ExtractEquiv.lean
@@ -116,7 +116,6 @@ lemma sum_extractEquiv_congr [AddCommMonoid M] {n m : ℕ} (i : Fin n) (f : Wick
 
 The proof of this result uses the fact that Lean is an executable programming language
 and can calculate all Wick contractions for a given `n`. -/
-@[sorryful]
 lemma mem_three (c : WickContraction 3) : c.1 ∈ ({∅, {{0, 1}}, {{0, 2}}, {{1, 2}}} :
     Finset (Finset (Finset (Fin 3)))) := by
   revert c
@@ -133,7 +132,6 @@ lemma mem_three (c : WickContraction 3) : c.1 ∈ ({∅, {{0, 1}}, {{0, 2}}, {{1
 The proof of this result uses the fact that Lean is an executable programming language
 and can calculate all Wick contractions for a given `n`.
 -/
-@[sorryful]
 lemma mem_four (c : WickContraction 4) : c.1 ∈ ({∅,
     {{0, 1}}, {{0, 2}}, {{0, 3}}, {{1, 2}}, {{1, 3}}, {{2,3}},
     {{0, 1}, {2, 3}}, {{0, 2}, {1, 3}}, {{0, 3}, {1, 2}}} :
