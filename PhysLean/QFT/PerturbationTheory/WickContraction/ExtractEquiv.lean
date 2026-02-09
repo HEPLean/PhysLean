9fefb57b36738945ab0642c0c9ec45f9f92f530b
ZhiKai Pong <zhikaip@gmail.com>
2026-02-09 19:13:58 +0000

    extra simp

--- PhysLean/QFT/PerturbationTheory/WickContraction/ExtractEquiv.lean
+++ PhysLean/QFT/PerturbationTheory/WickContraction/ExtractEquiv.lean
@@ -139,7 +139,6 @@ lemma mem_four (c : WickContraction 4) : c.1 ∈ ({∅,
     {{0, 1}, {2, 3}}, {{0, 2}, {1, 3}}, {{0, 3}, {1, 2}}} :
     Finset (Finset (Finset (Fin 4)))) := by
   revert c
-  simp
   decide +kernel
 
 end WickContraction
