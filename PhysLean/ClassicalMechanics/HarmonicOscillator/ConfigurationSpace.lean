5d7c61941e5e41698f6c74ed535ff99f686bea5e
Nicola Bernini <nicola.bernini@gmail.com>
2026-02-17 20:49:09 +0100

    fix: Fixing Author

--- PhysLean/ClassicalMechanics/HarmonicOscillator/ConfigurationSpace.lean
+++ PhysLean/ClassicalMechanics/HarmonicOscillator/ConfigurationSpace.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2026 Nicola Bernini. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Nicola Bernini, Lode Vermeulen
+Authors: Nicola Bernini
 -/
 import Mathlib.Analysis.InnerProductSpace.Basic
 import Mathlib.Geometry.Manifold.Diffeomorph
